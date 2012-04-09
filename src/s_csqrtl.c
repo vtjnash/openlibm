cc1f15ef5160a5118ad9c663f0443cae753c8f11
Jameson Nash <vtjnash@gmail.com>
2012-04-08 20:03:36 -0400

    initial attempt getting everything to build on windows

--- src/s_csqrtl.c
+++ src/s_csqrtl.c
@@ -24,7 +24,7 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
+#include "cdefs-compat.h"
 //__FBSDID("$FreeBSD: src/lib/msun/src/s_csqrtl.c,v 1.2 2008/08/08 00:15:16 das Exp $");
 
 #include <complex.h>
